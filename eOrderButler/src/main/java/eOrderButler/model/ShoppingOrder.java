e1224c39234b4d77572efb63cc3730227648c26e
nac95 <50491534+nac95@users.noreply.github.com>
2020-05-23 00:56:07 -0500

    Add files via upload

---
+++ eOrderButler/src/main/java/eOrderButler/model/ShoppingOrder.java
@@ -0,0 +1,104 @@
+package eOrderButler.model;
+
+import java.io.Serializable;
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ShoppingOrder")
+public class ShoppingOrder implements Serializable {
+
+	private static final long serialVersionUID = -5463145618287767986L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int orderId;
+	
+	@ManyToOne
+	private int userId;
+	
+	private String orderNumber;
+	private String merchant;
+	private String status;
+	private Date date;
+	private double totalPrice;
+	
+	@OneToMany(mappedBy = "order", cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+	private List<Item> items;
+
+	public int getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+
+	public int getuserId() {
+		return userId;
+	}
+
+	public void setuserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+
+	public String getMerchant() {
+		return merchant;
+	}
+
+	public void setMerchant(String merchant) {
+		this.merchant = merchant;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public double getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void setTotalPrice(double totalPrice) {
+		this.totalPrice = totalPrice;
+	}
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Item> items) {
+		this.items = items;
+	}
+
+	
+}
