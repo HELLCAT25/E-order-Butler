906ced92d413990619d392918461bc1c3b79e12a
HELLCAT25 <michael_z97@hotmail.com>
2020-06-06 21:37:45 -0500

    Update ShoppingOrderDao.java

--- eOrderButler/src/main/java/eOrderButler/dao/ShoppingOrderDao.java
+++ eOrderButler/src/main/java/eOrderButler/dao/ShoppingOrderDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 
 import org.hibernate.Session;
